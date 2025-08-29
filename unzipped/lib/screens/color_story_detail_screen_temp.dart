b9d5e1fdef2af2527de763687ebdd719dd6a3406
Corey Hamilton <colrvia@gmail.com>
2025-08-28 20:51:25 -0500

    Remove extra unzipped folder

--- unzipped/lib/screens/color_story_detail_screen_temp.dart
+++
@@ -1,1 +0,0 @@
-// This file has been replaced and can be removed
--- unzipped/test/role_order_test.dart
+++
@@ -1,3 +0,0 @@
-// This file has been removed as part of Designer role elimination.
-// Role-based functionality has been replaced with simple palette size selection (1-9).
-// See designer_size_test.dart for new size-based tests.
